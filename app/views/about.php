7e5d5005821c52a0aa5e3fc921507bde99a5cf63
Regular-Stone <132766689+Regular-Stone@users.noreply.github.com>
2024-05-18 22:03:58 +0200

    about et event page

--- app/views/about.php
+++ app/views/about.php
@@ -1,7 +1,50 @@
+<style>
+    .about {
+        margin: 0 auto;
+        width: 70%;
+        text-align: justify;
+        margin-top: 6rem;
+    }
 
-<section style="padding-top: 15vh">
-    <div class="container">
-        <h1>About</h1>
-        <p>This is a simple PHP MVC framework.</p>
+    .about h1 {
+        font-size: 2em;
+        text-align: center;
+        margin-bottom: 4rem;
+    }
+
+    .about h2 {
+        font-size: 1.5em;
+        margin-bottom: 2rem;
+        text-align: center ;
+    }
+
+    .about p {
+        font-size: 1em;
+        margin-bottom: 4rem;
+
+    }
+
+    .about .parteners {
+        display: flex;
+        justify-content: center;
+
+        img {
+            width: 20%;
+            margin: 0 1rem;
+        }
+    }
+</style>
+
+<section class="main-content">
+    <div class="about">
+        <h1>A propos de nous...</h1>
+        <h2>L'association</h2>
+        <p>Binary Eagle est une association Niçoise créée le 23 février 2020, à but non lucratif (se référant à la loi 1901). Elle a comme vocation d’agir dans le secteur de l’E-sport, par différentes actions (évènements/ LineUp). L’origine de sa création est due à deux constats. Tout d’abord celui lié à l’image de l'e-sport français en France et dans le monde, qui, alors que dans un premier temps se faisait timide, est actuellement entrain de progressivement émerger grâce à des évènements ponctuels ou réguliers associés à certaines villes. Et ensuite par le fait que contrairement à d'autres villes Françaises comme Montpellier, Nice n'a pas d'évènement e-sport majeur.</p>
+        <h2>Nos objectifs</h2>
+        <p>Binary Eagle a pour objectif de promouvoir l’e-sport à Nice et dans les alentours, en organisant des évènements, en créant une LineUp compétitive et en proposant des services de coaching. L’association a pour ambition de devenir un acteur majeur de l’e-sport dans la région, en proposant des évènements de qualité et en participant à des compétitions nationales et internationales.</p>
+        <h2>Notre équipe</h2>
+        <p>Binary Eagle est composée de 3 membres fondateurs, tous passionnés par l’e-sport et ayant une expérience dans le domaine. L’équipe est complétée par des membres actifs, qui participent à l’organisation des évènements et à la gestion de la LineUp. L’association est également soutenue par des partenaires locaux, qui permettent de financer les évènements et de proposer des lots aux participants.</p>
+        <h2>Nos partenaires</h2>
+        <p class="parteners"><img src="/binary_back/public/imgs/fox.jpg" alt=""></p>
     </div>
 </section>
